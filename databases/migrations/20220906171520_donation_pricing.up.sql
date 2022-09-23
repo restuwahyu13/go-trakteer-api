75f83da4a3116671d76e8eaf0b49b39a92f99059
restuwahyu13 <restuwahyu13@gmail.com>
2022-09-23 15:55:30 +0700

    add new migration database and change relation for users to customers

--- databases/migrations/20220906171520_donation_pricing.up.sql
+++ databases/migrations/20220906171520_donation_pricing.up.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS "donation_pricing" (
   "id" serial PRIMARY KEY,
-  "user_id" int NOT NULL UNIQUE,
+  "customer_id" int NOT NULL UNIQUE,
   "donation_type_id" int NOT NULL,
   "amount" bigint NOT NULL,
   "created_at" timestamp NULL DEFAULT (now()),
   "updated_at" timestamp NULL DEFAULT (now())
 );
 
-ALTER TABLE "donation_pricing" ADD FOREIGN KEY (user_id) REFERENCES "customers" ON DELETE CASCADE;
+ALTER TABLE "donation_pricing" ADD FOREIGN KEY (customer_id) REFERENCES "customers" ON DELETE CASCADE;
 ALTER TABLE "donation_pricing" ADD FOREIGN KEY (donation_type_id) REFERENCES "donation_type" ON DELETE CASCADE;
--- databases/migrations/20220907144615_users_payment.up.sql
+++ databases/migrations/20220907144615_users_payment.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS "users_payment" (
   "id" serial PRIMARY KEY,
-  "user_id" int NOT NULL UNIQUE,
+  "customer_id" int NOT NULL UNIQUE,
   "balance_id" int NOT NULL UNIQUE,
   "wallet_id" int NOT NULL UNIQUE
 );
 
-ALTER TABLE "users_payment" ADD FOREIGN KEY (user_id) REFERENCES "customers" ON DELETE RESTRICT;
+ALTER TABLE "users_payment" ADD FOREIGN KEY (customer_id) REFERENCES "customers" ON DELETE RESTRICT;
 ALTER TABLE "users_payment" ADD FOREIGN KEY (balance_id) REFERENCES "balance" ON DELETE RESTRICT;
 ALTER TABLE "users_payment" ADD FOREIGN KEY (wallet_id) REFERENCES "wallet" ON DELETE RESTRICT;
